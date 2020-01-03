a3b025f0338ee7fd4100207bc0e6809574cd0356
Steve McLaughlin <stevejrmc@gmail.com>
2020-01-02 17:21:13 -0800

    Add mongo and postgres server--postgres server is a template/incomplete

---
+++ server/pg.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const Controller = require('../db/controller')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/../client/dist'))
+
+app.get('/reviews', Controller.get)
+app.get('/reviews/:id', Controller.getOne)
+app.post('/reviews', Controller.post)
+app.put('/reviews/:id', Controller.put)
+app.delete('/reviews/:id', Controller.delete)
+
+app.listen(3003, () => console.log('app listening on port 3003'))
