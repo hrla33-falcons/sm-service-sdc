03718176e1a869327c87e98407e79f9dff7b063b
Steve McLaughlin <stevejrmc@gmail.com>
2020-01-11 12:02:26 -0800

    Add loaderio verification route and file

--- server/mg.js
+++ server/mg.js
@@ -9,6 +9,11 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/../client/dist'))
 
+// loaderio verification
+app.get('/loaderio-84f46b8a0bcf09f289e60658c7a7f157', (req, res) => {
+  res.status(200).sendFile(__dirname + '/loaderio-84f46b8a0bcf09f289e60658c7a7f157.txt')
+})
+
 app.get('/reviews', Controller.get)
 app.get('/reviews/:id', Controller.getOne)
 app.post('/reviews', Controller.post)
